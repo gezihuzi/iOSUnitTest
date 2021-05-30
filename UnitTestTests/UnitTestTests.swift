91fdafc15d03b7bfcee6c14bade05612b1eae508
leavesster <11785335+leavesster@users.noreply.github.com>
2021-05-30 20:20:41 +0800

    download test

--- UnitTestTests/UnitTestTests.swift
+++ UnitTestTests/UnitTestTests.swift
@@ -6,10 +6,24 @@
 //
 
 import XCTest
+
 @testable import UnitTest
+
+class DownloadHandler: NSObject {
+    let session: URLSession = URLSession(configuration: .default)
+    public func download(_ request: URLRequest, completionHandler: @escaping (_ success: Bool, URL?, URLResponse?, Error?) -> Void) {
+        let task = session.downloadTask(with: request) { url, response, error in
+            let success = true
+            completionHandler(success, url, response, error)
+            print(response ?? "no response")
+        }
+        task.resume()
+    }
+}
 
 class UnitTestTests: XCTestCase {
 
+    let downloader = DownloadHandler()
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -17,10 +31,71 @@ class UnitTestTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    func testDownload() {
+        let expectation = self.expectation(description: "\(#function)")
+        
+        let request = URLRequest(url: URL(string: "https://api.github.com/users")!)
+        
+        downloader.download(request) { success, url, res, err in
+            print("url: \(String(describing: url?.absoluteString)), res: \(String(describing: res)), err: \(String(describing: err))")
+            expectation.fulfill()
+        }
+        
+        self.waitForExpectations(timeout: 10) { error in
+            if let e = error {
+                print("\(e)")
+            }
+        }
+    }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testRemovePreviousDownload() throws {
+    
+        let expectation = self.expectation(description: "\(#function)")
+        
+        let request = URLRequest(url: URL(string: "https://api.github.com/users")!)
+        
+        downloader.download(request) { success, url, res, err in
+            print("url: \(String(describing: url?.absoluteString)), res: \(String(describing: res)), err: \(String(describing: err))")
+            XCTFail()
+        }
+        
+        
+        downloader.download(request) { success, url, res, err in
+            print("url: \(String(describing: url?.absoluteString)), res: \(String(describing: res)), err: \(String(describing: err))")
+            expectation.fulfill()
+        }
+        
+        
+        self.waitForExpectations(timeout: 10) { error in
+            if let e = error {
+                print("\(e)")
+            }
+        }
+        
+    }
+    
+    func testFailDownload() throws {
+        let expectation = self.expectation(description: "\(#function)")
+        
+        let request = URLRequest(url: URL(string: "https://api.github.com/users11111")!)
+        
+        downloader.download(request) { success, url, res, err in
+            print("url: \(String(describing: url?.absoluteString)), res: \(String(describing: res)), err: \(String(describing: err))")
+            if success {
+                XCTFail()
+            } else {
+                expectation.fulfill()
+            }
+        }
+        
+        
+        self.waitForExpectations(timeout: 10) { error in
+            if let e = error {
+                print("\(e)")
+            }
+        }
+
     }
 
     func testPerformanceExample() throws {
